a6bfa295719a80720a4764a9ab3297a51f452bbc
1905-ActivityWeb <smileful3@gmail.com>
2019-08-02 15:39:34 +0800

    190802-HTML

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,6 @@
 'use strict';
 
-$(document).ready(function () {
-
+$(window).on('load', function () {
     if ($(window).width() > 750) {
 
         $('.ImgItemBox').addClass('aaa');
@@ -88,9 +87,33 @@ $(document).ready(function () {
             // 每次捲動畫面都要一定間隔
             setTimeout(function () {
                 canMove = true;
-            }, 500);
+            }, 1500);
         });
     } else {
-        $('.PageBox').addClass('animation');
+        var PageBoxPosition = [];
+        var windowHeight = $(window).height();
+        $('.PageBox').each(function (i, ele) {
+            var position = $(ele).offset().top - windowHeight / 2;
+            if (position > 0) {
+                PageBoxPosition.push(position);
+            } else {
+                PageBoxPosition.push(0);
+            }
+        });
+        $(window).on('scroll', function (event) {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > PageBoxPosition[2]) {
+                $('.PageBox').eq(2).addClass('animation');
+            }
+            if (scrollTop > PageBoxPosition[3]) {
+                $('.PageBox').eq(3).addClass('animation');
+            }
+            if (scrollTop > PageBoxPosition[4]) {
+                $('.PageBox').eq(4).addClass('animation');
+            }
+            if (scrollTop > PageBoxPosition[5]) {
+                $('.PageBox').eq(5).addClass('animation');
+            }
+        });
     }
 });
